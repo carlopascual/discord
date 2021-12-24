0d64c962edd1f2166583a253af10b045d62c715c
Carlo Pascual <carlopascual@yahoo.com>
2021-12-24 03:24:18 +0100

    Move normalize.css to public folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import type { AppProps } from "next/app";
 import GlobalStyle from "styles/global";
+import "../../public/normalize.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="http://yui.yahooapis.com/3.11.0/build/cssnormalize/cssnormalize-min.css"
-      />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
